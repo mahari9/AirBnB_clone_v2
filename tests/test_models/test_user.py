133cc2d3f14dd8c1b5f78e63b96e7cb6428aa82a
Mahari9 <merytseg77@gmail.com>
2024-04-17 05:43:57 -0700

    Check on tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,48 +1,34 @@
 #!/usr/bin/python3
-"""This module contains unit tests for the User class."""
-import os
-from sqlalchemy import Column
-
-from tests.test_models.test_base_model import TestBasemodel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
-class TestUser(TestBasemodel):
-    """TestUser class for testing the User model."""
+class test_User(test_basemodel):
+    """ """
+
     def __init__(self, *args, **kwargs):
-        """Initializes the test class."""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """Tests the first_name attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(
-            type(new.first_name),
-            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
-        )
+        self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """Tests the last_name attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(
-            type(new.last_name),
-            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
-        )
+        self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """Tests the email attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(
-            type(new.email),
-            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
-        )
+        self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """Tests the password attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(
-            type(new.password),
-            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
-        )
+        self.assertEqual(type(new.password), str)
