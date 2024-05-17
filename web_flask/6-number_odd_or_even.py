01acd3e486c459eda61a5dd91e2db7247bb14411
Mahari9 <merytseg77@gmail.com>
2024-05-17 09:24:28 -0700

    Web framework

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application.
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
