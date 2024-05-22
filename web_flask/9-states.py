aebf85885f4b40d673ee6ca53af9ee835488117f
Mahari9 <merytseg77@gmail.com>
2024-05-22 13:54:59 -0700

    States

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,31 +9,19 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
 def states(id=None):
     """The states page."""
-    states = None
-    state = None
-    all_states = list(storage.all(State).values())
-    case = 404
-    if id is not None:
-        res = list(filter(lambda x: x.id == id, all_states))
-        if len(res) > 0:
-            state = res[0]
-            state.cities.sort(key=lambda x: x.name)
-            case = 2
-    else:
-        states = all_states
-        for state in states:
-            state.cities.sort(key=lambda x: x.name)
-        states.sort(key=lambda x: x.name)
-        case = 1
-    ctxt = {
-        'states': states,
-        'state': state,
-        'case': case
-    }
-    return render_template('9-states.html', **ctxt)
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """The state_id page"""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
