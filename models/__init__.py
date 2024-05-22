03a808ba9a7d439aa5e922440d36f491de67026b
Mahari9 <merytseg77@gmail.com>
2024-05-22 13:18:41 -0700

    storage

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,20 @@
 application.
 A unique FileStorage/DBStorage instance for all models
 """
-import os
-
-from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
 
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
 
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
