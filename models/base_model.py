477919f4c6320499b4d9d53efc57b961a8a6d6cd
Mahari9 <merytseg77@gmail.com>
2024-05-23 05:19:59 -0700

    BASEMODEL

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,11 @@ BaseModel which is a base model for classes with same/common
 attributes and methods in the HBnB project and defines all common
 attributes/methods for other classes.
 """
-import os
-import uuid
-from datetime import datetime
-from sqlalchemy import Column, String, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
+import uuid
+import models
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
 
 Base = declarative_base()
@@ -16,66 +16,53 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
-        """Instantiates base model class,
-        Args:
-            kwargs: arguments for the constructor of the BaseModel
-        Attributes:
-            id: unique id generated
-            created_at: creation date
-            updated_at: updated date
-        """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        """Instantiation of base model class"""
+        if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
-            if not hasattr(kwargs, 'id'):
-                setattr(self, 'id', str(uuid.uuid4()))
-            if not hasattr(kwargs, 'created_at'):
-                setattr(self, 'created_at', datetime.now())
-            if not hasattr(kwargs, 'updated_at'):
-                setattr(self, 'updated_at', datetime.now())
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string represtentation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """return a string representaion"""
+        return self.__str__()
 
     def save(self):
         """Updates with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """creates dictionary of the class  and returns
-        Return:
-            returns a dictionary of all the key values in __dict__
-        """
-        bm_dict = {}
-        for key, value in self.__dict__.items():
-            if key != '_sa_instance_state':
-                if isinstance(value, datetime):
-                    bm_dict[key] = value.isoformat()
-                else:
-                    bm_dict[key] = value
-        bm_dict['__class__'] = self.__class__.__name__
+        """creates dictionary of the class  and returns"""
+        bm_dict = dict(self.__dict__)
+        bm_dict["__class__"] = str(type(self).__name__)
+        bm_dict["created_at"] = self.created_at.isoformat()
+        bm_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in bm_dict.keys():
+            del bm_dict['_sa_instance_state']
         return bm_dict
 
     def delete(self):
-        """Deletes the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
+        """ delete object"""
+        models.storage.delete(self)
